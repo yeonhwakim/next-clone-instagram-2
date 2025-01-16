35506d0176753657397e52203fb144a944f19edd
yeonhwa <yeonhwa6717@gmail.com>
2025-01-16 14:44:49 +0900

    Add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+
+export default async function HomePage() {
+  const session = await auth()
+  const user = session?.user
+
+  if (!user) return redirect('/signin')
+
   return (
-    <h1 className="text-gray-300">instagram</h1>
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
   );
 }
