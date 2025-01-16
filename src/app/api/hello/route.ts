4ff50a1736f645ec4e6b80acbbbc31de54cca606
yeonhwa <yeonhwa6717@gmail.com>
2025-01-16 15:51:37 +0900

    Set SWR

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,1 @@
+export async function GET(request: Request) { return Response.json('Hello Nextjs') }
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import useSWR from "swr"
+
 export default function FollowingBar() {
+  const { data, error } = useSWR('/api/hello')
+  console.log(data)
   return <p>FollowingBar</p>
 }
