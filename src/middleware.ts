b0272a4bac8234e8493a397cc9a3c3ad7070e07d
yeonhwa <yeonhwa6717@gmail.com>
2025-01-24 12:03:53 +0900

    reset middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,35 @@
-import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server';
+// import { getToken } from 'next-auth/jwt';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+// export async function middleware(req: NextRequest) {
+//   const token = await getToken({ req });
 
-  if (!token) {
-    if (req.nextUrl.pathname.startsWith('/api')) {
-      return new NextResponse('Authentication Error', { status: 401 });
-    }
+//   if (!token) {
+//     if (req.nextUrl.pathname.startsWith('/api')) {
+//       return new NextResponse('Authentication Error', { status: 401 });
+//     }
 
-    const { pathname, search, origin, basePath } = req.nextUrl;
-    const signInUrl = new URL(`${basePath}/signin`, origin);
-    signInUrl.searchParams.append(
-      'callbackUrl',
-      `${basePath}${pathname}${search}`
-    );
-    return NextResponse.redirect(signInUrl);
-  }
+//     const { pathname, search, origin, basePath } = req.nextUrl;
+//     const signInUrl = new URL(`${basePath}/signin`, origin);
+//     signInUrl.searchParams.append(
+//       'callbackUrl',
+//       `${basePath}${pathname}${search}`
+//     );
+//     return NextResponse.redirect(signInUrl);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    '/new',
-    '/',
-    '/api/bookmarks',
-    '/api/comments',
-    '/api/likes',
-    '/api/follow',
-    '/api/me',
-    '/api/posts/:path*',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/new',
+//     '/',
+//     '/api/bookmarks',
+//     '/api/comments',
+//     '/api/likes',
+//     '/api/follow',
+//     '/api/me',
+//     '/api/posts/:path*',
+//   ],
+// };
